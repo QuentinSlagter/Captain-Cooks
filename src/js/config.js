44569bb01f9799ff2a2d0d6dc2954a59e80d275b
QuentinSlagter <93204007+QuentinSlagter@users.noreply.github.com>
2022-02-19 17:53:46 -0600

    first commit of Captain Cooks

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '65af3a37-933f-4498-990e-715329600118';
+export const MODAL_CLOSE_SEC = 2.5;
